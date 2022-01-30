5f436515b8047e17a72ffc3f3b1fb5e0dc2665b8
Yannick Sereckissy <yannick.sereckissy@gmail.com>
2022-01-30 15:02:55 +0100

    delete, clear and filter task list

--- task-list/app.js
+++ task-list/app.js
@@ -8,6 +8,12 @@ loadEventListeners();
 
 function loadEventListeners() {
     form.addEventListener('submit', addTask);
+    /// remove task event
+    taskList.addEventListener('click', removeTask)
+    /// clear task
+    clearBtn.addEventListener('click', clearTask);
+    /// filter tasks
+    filter.addEventListener('keyup', filterTask);
 }
 
 function addTask(e) {
@@ -26,4 +32,31 @@ function addTask(e) {
     /// append the li to the ul
     taskList.appendChild(li);
     taskInput.value = '';
+}
+
+function removeTask(e) {
+    e.preventDefault();
+    if(e.target.parentElement.classList.contains('delete-item')) {
+        if(confirm('Are you sure?'))
+        e.target.parentElement.parentElement.remove();
+    }
+}
+
+function clearTask(e) {
+    while (taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild);
+    }
+}
+
+function filterTask(e) {
+    const text = e.target.value.toLowerCase();
+    document.querySelectorAll('.collection-item').forEach(task => {
+        const item = task.firstChild.textContent;
+        console.log(item)
+        if(-1 !== item.toLowerCase().indexOf(text)) {
+            task.style.display = 'block';
+        } else {
+            task.style.display = 'none';
+        }
+    })
 }
